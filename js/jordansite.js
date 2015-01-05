d8d12f85823eb7b76ec24f2b34f7e161518985f6
jordanwilcken@github.com <jordanwilcken@gmail.com>
2015-01-05 11:02:28 -0700

    landing page is set

---
+++ js/jordansite.js
@@ -0,0 +1,94 @@
+/*
+ * module_template.js
+ * Template for browser feature modules
+ *
+ * Michael S. Mikowski - mike.mikowski@gmail.com
+ * Copyright (c) 2011-2012 Manning Publications Co.
+*/
+
+/*jslint         browser : true, continue : true,
+  devel  : true, indent  : 2,    maxerr   : 50,
+  newcap : true, nomen   : true, plusplus : true,
+  regexp : true, sloppy  : true, vars     : false,
+  white  : true
+*/
+
+/*global $ */
+
+var jordansite = (function () {
+  'use strict';
+  //---------------- BEGIN MODULE SCOPE VARIABLES --------------
+  var
+    configMap = {},
+    stateMap  = {},
+    jqueryMap = {},
+
+    setJqueryMap, configModule, initModule, sizeTheImage;
+  //----------------- END MODULE SCOPE VARIABLES ---------------
+
+  //------------------- BEGIN UTILITY METHODS ------------------
+  // example : getTrimmedString
+  //-------------------- END UTILITY METHODS -------------------
+
+  //--------------------- BEGIN DOM METHODS --------------------
+  // Begin DOM method /setJqueryMap/
+  setJqueryMap = function () {
+    var $container = stateMap.$container;
+
+    jqueryMap = {
+      $container    : $container,
+      $allMyContent : $container.find("#all-my-content"),
+      $theImage     : $container.find("#pretty-image")
+    };
+  };
+  // End DOM method /setJqueryMap/
+
+  // Begin DOM method /sizeTheImage/
+  sizeTheImage = function () {
+    jqueryMap.$theImage.css("width", jqueryMap.$container.css("max-width"));
+  };
+  // End DOM method /sizeTheImage/
+  //---------------------- END DOM METHODS ---------------------
+
+  //------------------- BEGIN EVENT HANDLERS -------------------
+  // example: onClickButton = ...
+  //-------------------- END EVENT HANDLERS --------------------
+
+
+
+  //------------------- BEGIN PUBLIC METHODS -------------------
+  // Begin public method /configModule/
+  // Purpose    : Adjust configuration of allowed keys
+  // Arguments  : A map of settable keys and values
+  // Settings   :
+  //   * configMap.settable_map declares allowed keys
+  // Returns    : true
+  // Throws     : none
+  //
+  configModule = function ( input_map ) {
+    return true;
+  };
+  // End public method /configModule/
+
+  // Begin public method /initModule/
+  // Purpose    : Initializes module
+  // Arguments  :
+  //  * $container the jquery element used by this feature
+  // Returns    : true
+  // Throws     : none
+  //
+  initModule = function ( $container ) {
+    stateMap.$container = $container;
+    setJqueryMap();
+    sizeTheImage();
+    return true;
+  };
+  // End public method /initModule/
+
+  // return public methods
+  return {
+    configModule : configModule,
+    initModule   : initModule
+  };
+  //------------------- END PUBLIC METHODS ---------------------
+}());
